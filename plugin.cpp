9c35f4fe482c2eaaba6e267e044144c2486b5d92
Tanjim Rahat <tanjim.rahat.dipto@gmail.com>
2025-08-17 06:40:40 +0600

    Added logger but not working

--- plugin.cpp
+++ plugin.cpp
@@ -1,11 +1,30 @@
+#include <spdlog/sinks/basic_file_sink.h>
+
+namespace logger = SKSE::log;
+
+void SetupLog() {
+    auto logsFolder = SKSE::log::log_directory();
+    if (!logsFolder) SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
+    auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
+    auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
+    auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
+    auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+    spdlog::set_default_logger(std::move(loggerPtr));
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::flush_on(spdlog::level::trace);
+}
+
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
+    SetupLog();
+    logger::info("Hello, world from TestTRD!");
 
     // Once all plugins and mods are loaded, then the ~ console is ready and can
     // be printed to
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
-        if (message->type == SKSE::MessagingInterface::kDataLoaded)
+        if (message->type == SKSE::MessagingInterface::kDataLoaded) {
             RE::ConsoleLog::GetSingleton()->Print("Hello, world from TestTRD!");
+        }
     });
 
     return true;
